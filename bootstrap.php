2e468c57372515620959ec5b7b9ee15e71239e2c
manse <info@manse.jp>
2013-04-03 16:51:48 +0900

    Fixed not working bug on modx evo 1.0.10

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,10 @@
 if ($modx->event->name === 'OnManagerLoginFormRender' && is_file(MODX_BASE_PATH . $plugin_base . 'template.html')) {
 	$modx->setPlaceholder('ml_year', date('Y'));
 	$modx->setPlaceholder('ml_http_path', $modx->config['base_url'] . $plugin_base);
-	$GLOBALS['tpl'] = file_get_contents(MODX_BASE_PATH . $plugin_base . 'template.html');
+	$tpl = file_get_contents(MODX_BASE_PATH . $plugin_base . 'template.html');
+	$tpl = $modx->mergePlaceholderContent($tpl);
+	$regx = strpos($tpl,'[[+') !== false ? '~\[\[\+(.*?)\]\]~' : '~\[\+(.*?)\+\]~';
+	$tpl = preg_replace($regx, '', $tpl);
+	echo $tpl;
+	exit;
 }
